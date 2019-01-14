c4431e281edcafbdd37e4cad2b0b3c12423f8e66
yapjinai <jinai.jyap@gmail.com>
2019-01-14 14:22:56 -0500

    fetching from API. next step : figure out how to connect to auth in backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
+const apiUrl = 'http://localhost:3000'
+
 class Home extends Component {
   render() {
+    this.fetchUsers()
+
     return (
       <div className="Home">
         Home
       </div>
     );
+  }
+
+  ///////////////////////
+
+  fetchUsers = () => {
+    fetch(`${apiUrl}/api/v1/users`)
+    .then(r => r.json())
+    .then(console.log)
   }
 }
 
