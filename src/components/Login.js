91598708e5890d13cd964de11c0f114b6f880fa6
yapjinai <jinai.jyap@gmail.com>
2019-01-14 14:51:14 -0500

    working on login step

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,13 +12,28 @@ class Login extends Component {
     return (
       <div className="Login">
         Login
-        <form>
+        <form
+          id='loginForm'
+          onSubmit={this.handleSubmit}
+        >
           <input
             id='username'
             type='text'
             placeholder='username'
             value={this.state.username}
-            onChange={this.handleUsernameChange}
+            onChange={this.handleChange}
+          />
+          <input
+            id='password'
+            type='text'
+            placeholder='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <input
+            id='submit'
+            type='submit'
+            value='Log in'
           />
         </form>
       </div>
@@ -27,10 +42,35 @@ class Login extends Component {
 
   ///////////////////////
 
-  handleUsernameChange = (e) => {
+  handleChange = (e) => {
     this.setState({
-      username: e.target.value
+      [e.target.id]: e.target.value
     })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const username = e.target.querySelector('#username').value
+    const password = e.target.querySelector('#password').value
+
+    fetch(`${apiUrl}/api/v1/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        }
+      })
+    })
+    .then(r => r.json())
+    .then(r => {
+      console.log('console:', r)
+    })
+
   }
 
 }
