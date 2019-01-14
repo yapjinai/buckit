485b459d7bbe32cb2ae7a9fec832fefa8e837935
yapjinai <jinai.jyap@gmail.com>
2019-01-14 14:18:37 -0500

    connected react router

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="Nav">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/profile'>Profile</Link></li>
+          <li><Link to='/items'>Items</Link></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
