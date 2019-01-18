8a9c60d0fc9360f1adf8703ecf9f525a28ca004d
yapjinai <jinai.jyap@gmail.com>
2019-01-18 17:46:20 -0500

    made nav indicate logged in status

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
+// Redux
+import { connect } from 'react-redux'
+
 class Nav extends Component {
   render() {
     return (
       <nav className="Nav">
+      {this.props.loggedIn ? `Logged in as ${this.props.user.display_name}` : 'please log in'}
         <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/login'>Login</Link></li>
@@ -16,4 +20,19 @@ class Nav extends Component {
   }
 }
 
-export default Nav;
+///////////////
+// redux
+///////////////
+
+const mapStateToProps = (state) => {
+  return ({
+    loggedIn: state.UsersReducer.loggedIn,
+    user: state.UsersReducer.user
+  })
+}
+
+const connectedNav = connect(
+  mapStateToProps
+)(Nav)
+
+export default connectedNav;
